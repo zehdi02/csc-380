95e60046235439818567240428b30fe4626802ac
Alhamza Muswara <hamza.muswara@gmail.com>
2023-04-16 16:57:58 -0400

    public key exchange

--- chat.cpp
+++ chat.cpp
@@ -22,16 +22,22 @@ using std::deque;
 using std::pair;
 #include "dh.h"
 
-static pthread_t trecv;     /* wait for incoming messagess and post to queue */
-void* recvMsg(void*);       /* for trecv */
-static pthread_t tcurses;   /* setup curses and draw messages from queue */
-void* cursesthread(void*);  /* for tcurses */
+mpz_t sk_server, pk_server;
+mpz_t sk_client, pk_client;
+
+
+
+static pthread_t trecv;		/* wait for incoming messagess and post to queue */
+void *recvMsg(void *);		/* for trecv */
+static pthread_t tcurses;	/* setup curses and draw messages from queue */
+void *cursesthread(void *); /* for tcurses */
 /* tcurses will get a queue full of these and redraw the appropriate windows */
-struct redraw_data {
+struct redraw_data
+{
 	bool resize;
 	string msg;
 	string sender;
-	WINDOW* win;
+	WINDOW *win;
 };
 static deque<redraw_data> mq; /* messages and resizes yet to be drawn */
 /* manage access to message queue: */
@@ -43,7 +49,7 @@ static pthread_cond_t qcv = PTHREAD_COND_INITIALIZER;
 /* record chat history as deque of strings: */
 static deque<string> transcript;
 
-#define max(a, b)         \
+#define max(a, b) \
 	({ typeof(a) _a = a;    \
 	 typeof(b) _b = b;    \
 	 _a > _b ? _a : _b; })
@@ -69,17 +75,17 @@ int initServerNet(int port)
 	/* NOTE: might not need the above if you make sure the client closes first */
 	if (listensock < 0)
 		error("ERROR opening socket");
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+	bzero((char *)&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(port);
-	if (bind(listensock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+	if (bind(listensock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR on binding");
-	fprintf(stderr, "listening on port %i...\n",port);
-	listen(listensock,1);
+	fprintf(stderr, "listening on port %i...\n", port);
+	listen(listensock, 1);
 	socklen_t clilen;
-	struct sockaddr_in  cli_addr;
-	sockfd = accept(listensock, (struct sockaddr *) &cli_addr, &clilen);
+	struct sockaddr_in cli_addr;
+	sockfd = accept(listensock, (struct sockaddr *)&cli_addr, &clilen);
 	if (sockfd < 0)
 		error("error on accept");
 	close(listensock);
@@ -93,27 +99,27 @@ int initServerNet(int port)
 	NEWZ(pk_client);
 
 	size_t Bpklen = mpz_size(pk_server);
-    if (send(sockfd, pk_server, Bpklen, 0) == -1) {
-        error("ERROR sending DH public key");
-    }
+	if (send(sockfd, pk_server, Bpklen, 0) == -1)
+	{
+		error("ERROR sending DH public key");
+	}
 
 	size_t Bpklen = mpz_size(pk_server);
-	if (recv(sockfd, pk_server, Bpklen, 0) == -1) {
-	    error("ERROR receiving DH public key");
+	if (recv(sockfd, pk_server, Bpklen, 0) == -1)
+	{
+		error("ERROR receiving DH public key");
 	}
 	printf("Received DH server public key: %Zd\n", pk_server);
 
 	// Compute shared secret key
-    const size_t klen = 128;
-    unsigned char kA[klen];
-    dhFinal(sk_server, pk_server, pk_client, kA, klen);
-
+	const size_t klen = 128;
+	unsigned char kA[klen];
+	dhFinal(sk_server, pk_server, pk_client, kA, klen);
 
 	return 0;
 }
 
-
-static int initClientNet(char* hostname, int port)
+static int initClientNet(char *hostname, int port)
 {
 	struct sockaddr_in serv_addr;
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -121,15 +127,16 @@ static int initClientNet(char* hostname, int port)
 	if (sockfd < 0)
 		error("ERROR opening socket");
 	server = gethostbyname(hostname);
-	if (server == NULL) {
-		fprintf(stderr,"ERROR, no such host\n");
+	if (server == NULL)
+	{
+		fprintf(stderr, "ERROR, no such host\n");
 		exit(0);
 	}
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+	bzero((char *)&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+	memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
 	serv_addr.sin_port = htons(port);
-	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR connecting");
 	/* at this point, should be able to send/recv on sockfd */
 
@@ -145,7 +152,8 @@ static int initClientNet(char* hostname, int port)
 	/* Send public key to server */
 	unsigned char pk_client_buf[pLen];
 	Z2BYTES(pk_client_buf, pLen, pk_client);
-	if (send(sockfd, pk_client_buf, pLen, 0) == -1) {
+	if (send(sockfd, pk_client_buf, pLen, 0) == -1)
+	{
 		perror("client: send");
 		close(sockfd);
 		return -1;
@@ -153,7 +161,8 @@ static int initClientNet(char* hostname, int port)
 
 	/* Receive public key from server */
 	unsigned char pk_yours_buf[pLen];
-	if (recv(sockfd, pk_yours_buf, pLen, 0) == -1) {
+	if (recv(sockfd, pk_yours_buf, pLen, 0) == -1)
+	{
 		perror("client: recv");
 		close(sockfd);
 		return -1;
@@ -162,27 +171,28 @@ static int initClientNet(char* hostname, int port)
 	size_t pklen = mpz_size(pk_client);
 	const size_t klen = 128;
 	unsigned char kA[klen];
-	if (dhFinal(sk_client,pk_client,pk_server,kA,klen) == -1) {
+	if (dhFinal(sk_client, pk_client, pk_server, kA, klen) == -1)
+	{
 		fprintf(stderr, "diffie-hellman error\n");
 	}
-	
+
 	return 0;
 }
 
 static int shutdownNetwork()
 {
-	shutdown(sockfd,2);
+	shutdown(sockfd, 2);
 	unsigned char dummy[64];
 	ssize_t r;
-	do {
-		r = recv(sockfd,dummy,64,0);
+	do
+	{
+		r = recv(sockfd, dummy, 64, 0);
 	} while (r != 0 && r != -1);
 	close(sockfd);
 	return 0;
 }
 
 /* end network stuff. */
-
 
 [[noreturn]] static void fail_exit(const char *msg)
 {
@@ -196,10 +206,10 @@ static int shutdownNetwork()
 
 // Checks errors for (most) ncurses functions. CHECK(fn, x, y, z) is a checked
 // version of fn(x, y, z).
-#define CHECK(fn, ...) \
-	do \
-	if (fn(__VA_ARGS__) == ERR) \
-	fail_exit(#fn"("#__VA_ARGS__") failed"); \
+#define CHECK(fn, ...)                                  \
+	do                                                  \
+		if (fn(__VA_ARGS__) == ERR)                     \
+			fail_exit(#fn "(" #__VA_ARGS__ ") failed"); \
 	while (false)
 
 static bool should_exit = false;
@@ -221,38 +231,86 @@ static int readline_getc(FILE *dummy)
 
 /* if batch is set, don't draw immediately to real screen (use wnoutrefresh
  * instead of wrefresh) */
-static void msg_win_redisplay(bool batch, const string& newmsg="", const string& sender="")
+static void msg_win_redisplay(bool batch, const string &newmsg = "", const string &sender = "")
 {
 	if (batch)
 		wnoutrefresh(msg_win);
-	else {
-		wattron(msg_win,COLOR_PAIR(2));
-		wprintw(msg_win,"%s:",sender.c_str());
-		wattroff(msg_win,COLOR_PAIR(2));
-		wprintw(msg_win," %s\n",newmsg.c_str());
+	else
+	{
+		wattron(msg_win, COLOR_PAIR(2));
+		wprintw(msg_win, "%s:", sender.c_str());
+		wattroff(msg_win, COLOR_PAIR(2));
+		wprintw(msg_win, " %s\n", newmsg.c_str());
 		wrefresh(msg_win);
 	}
 }
 
 static void msg_typed(char *line)
 {
+	mpz_init(sk_server);
+	mpz_init(pk_server);
+	mpz_init(sk_client);
+	mpz_init(pk_client);
+	// dhGen(sk_client, pk_client);
+	//  send pk_client
+	//  Convert mpz_t variable pk to a string
+
+	ssize_t pk_size = mpz_sizeinbase(pk_client, 10); // need to add more space for MAC?
+
+	// Convert the mpz_t buffer to a string
+	char *pk_str = (char *)malloc(pk_size);
+	mpz_get_str(pk_str, 10, pk_client);
+
+	// Send the buffer size first
+	if (send(sockfd, &pk_size, sizeof(pk_size), 0) != sizeof(pk_size))
+	{
+		// Handle error
+	}
+
+	// Send the buffer itself
+	if (send(sockfd, pk_str, pk_size, 0) != pk_size)
+	{
+		// Handle error
+	}
+
+	// Convert the mpz_t buffer to a string
+	ssize_t y_pk_size;
+	if (recv(sockfd, &y_pk_size, sizeof(y_pk_size), 0) != sizeof(y_pk_size))
+	{
+		// handle error
+	}
+	char *y_pk_str = (char *)malloc(y_pk_size);
+
+	// receive buffer
+	if (recv(sockfd, y_pk_str, y_pk_size, 0) != y_pk_size)
+	{
+		// handle error
+	}
+	if (mpz_set_str(pk_server, y_pk_str, 10) < 0)
+	{
+		// handle error
+	}
 	string mymsg;
-	if (!line) {
+	if (!line)
+	{
 		// Ctrl-D pressed on empty line
 		should_exit = true;
 		/* XXX send a "goodbye" message so other end doesn't
 		 * have to wait for timeout on recv()? */
-	} else {
-		if (*line) {
+	}
+	else
+	{
+		if (*line)
+		{
 			add_history(line);
 			mymsg = string(line);
 			transcript.push_back("me: " + mymsg);
 			ssize_t nbytes;
-			if ((nbytes = send(sockfd,line,mymsg.length(),0)) == -1)
+			if ((nbytes = send(sockfd, line, mymsg.length(), 0)) == -1)
 				error("send failed");
 		}
 		pthread_mutex_lock(&qmx);
-		mq.push_back({false,mymsg,"me",msg_win});
+		mq.push_back({false, mymsg, "me", msg_win});
 		pthread_cond_signal(&qcv);
 		pthread_mutex_unlock(&qmx);
 	}
@@ -263,17 +321,20 @@ static void msg_typed(char *line)
 static void cmd_win_redisplay(bool batch)
 {
 	int prompt_width = strnlen(rl_display_prompt, 128);
-	int cursor_col = prompt_width + strnlen(rl_line_buffer,rl_point);
+	int cursor_col = prompt_width + strnlen(rl_line_buffer, rl_point);
 
 	werase(cmd_win);
 	mvwprintw(cmd_win, 0, 0, "%s%s", rl_display_prompt, rl_line_buffer);
 	/* XXX deal with a longer message than the terminal window can show */
-	if (cursor_col >= COLS) {
+	if (cursor_col >= COLS)
+	{
 		// Hide the cursor if it lies outside the window. Otherwise it'll
 		// appear on the very right.
 		curs_set(0);
-	} else {
-		wmove(cmd_win,0,cursor_col);
+	}
+	else
+	{
+		wmove(cmd_win, 0, cursor_col);
 		curs_set(1);
 	}
 	if (batch)
@@ -285,20 +346,21 @@ static void cmd_win_redisplay(bool batch)
 static void readline_redisplay(void)
 {
 	pthread_mutex_lock(&qmx);
-	mq.push_back({false,"","",cmd_win});
+	mq.push_back({false, "", "", cmd_win});
 	pthread_cond_signal(&qcv);
 	pthread_mutex_unlock(&qmx);
 }
 
 static void resize(void)
 {
-	if (LINES >= 3) {
-		wresize(msg_win,LINES-2,COLS);
-		wresize(sep_win,1,COLS);
-		wresize(cmd_win,1,COLS);
+	if (LINES >= 3)
+	{
+		wresize(msg_win, LINES - 2, COLS);
+		wresize(sep_win, 1, COLS);
+		wresize(cmd_win, 1, COLS);
 		/* now move bottom two to last lines: */
-		mvwin(sep_win,LINES-2,0);
-		mvwin(cmd_win,LINES-1,0);
+		mvwin(sep_win, LINES - 2, 0);
+		mvwin(cmd_win, LINES - 1, 0);
 	}
 
 	/* Batch refreshes and commit them with doupdate() */
@@ -313,7 +375,8 @@ static void init_ncurses(void)
 	if (!initscr())
 		fail_exit("Failed to initialize ncurses");
 
-	if (has_colors()) {
+	if (has_colors())
+	{
 		CHECK(start_color);
 		CHECK(use_default_colors);
 	}
@@ -324,11 +387,14 @@ static void init_ncurses(void)
 
 	curs_set(1);
 
-	if (LINES >= 3) {
+	if (LINES >= 3)
+	{
 		msg_win = newwin(LINES - 2, COLS, 0, 0);
 		sep_win = newwin(1, COLS, LINES - 2, 0);
 		cmd_win = newwin(1, COLS, LINES - 1, 0);
-	} else {
+	}
+	else
+	{
 		// Degenerate case. Give the windows the minimum workable size to
 		// prevent errors from e.g. wmove().
 		msg_win = newwin(1, COLS, 0, 0);
@@ -338,9 +404,10 @@ static void init_ncurses(void)
 	if (!msg_win || !sep_win || !cmd_win)
 		fail_exit("Failed to allocate windows");
 
-	scrollok(msg_win,true);
+	scrollok(msg_win, true);
 
-	if (has_colors()) {
+	if (has_colors())
+	{
 		// Use white-on-blue cells for the separator window...
 		CHECK(init_pair, 1, COLOR_WHITE, COLOR_BLUE);
 		CHECK(wbkgd, sep_win, COLOR_PAIR(1));
@@ -348,8 +415,9 @@ static void init_ncurses(void)
 		 * not appear to be any of the normal colors curses defines. */
 		CHECK(init_pair, 2, COLOR_MAGENTA, -1);
 	}
-	else {
-		wbkgd(sep_win,A_STANDOUT); /* c.f. man curs_attr */
+	else
+	{
+		wbkgd(sep_win, A_STANDOUT); /* c.f. man curs_attr */
 	}
 	wrefresh(sep_win);
 }
@@ -389,55 +457,59 @@ static void deinit_readline(void)
 	rl_callback_handler_remove();
 }
 
-static const char* usage =
-"Usage: %s [OPTIONS]...\n"
-"Secure chat for CSc380.\n\n"
-"   -c, --connect HOST  Attempt a connection to HOST.\n"
-"   -l, --listen        Listen for new connections.\n"
-"   -p, --port    PORT  Listen or connect on PORT (defaults to 1337).\n"
-"   -h, --help          show this message and exit.\n";
+static const char *usage =
+	"Usage: %s [OPTIONS]...\n"
+	"Secure chat for CSc380.\n\n"
+	"   -c, --connect HOST  Attempt a connection to HOST.\n"
+	"   -l, --listen        Listen for new connections.\n"
+	"   -p, --port    PORT  Listen or connect on PORT (defaults to 1337).\n"
+	"   -h, --help          show this message and exit.\n";
 
 int main(int argc, char *argv[])
 {
 	// define long options
 	static struct option long_opts[] = {
-		{"connect",  required_argument, 0, 'c'},
-		{"listen",   no_argument,       0, 'l'},
-		{"port",     required_argument, 0, 'p'},
-		{"help",     no_argument,       0, 'h'},
-		{0,0,0,0}
-	};
+		{"connect", required_argument, 0, 'c'},
+		{"listen", no_argument, 0, 'l'},
+		{"port", required_argument, 0, 'p'},
+		{"help", no_argument, 0, 'h'},
+		{0, 0, 0, 0}};
 	// process options:
 	char c;
 	int opt_index = 0;
 	int port = 1337;
-	char hostname[HOST_NAME_MAX+1] = "localhost";
+	char hostname[HOST_NAME_MAX + 1] = "localhost";
 	hostname[HOST_NAME_MAX] = 0;
 	bool isclient = true;
 
-	while ((c = getopt_long(argc, argv, "c:lp:h", long_opts, &opt_index)) != -1) {
-		switch (c) {
-			case 'c':
-				if (strnlen(optarg,HOST_NAME_MAX))
-					strncpy(hostname,optarg,HOST_NAME_MAX);
-				break;
-			case 'l':
-				isclient = false;
-				break;
-			case 'p':
-				port = atoi(optarg);
-				break;
-			case 'h':
-				printf(usage,argv[0]);
-				return 0;
-			case '?':
-				printf(usage,argv[0]);
-				return 1;
+	while ((c = getopt_long(argc, argv, "c:lp:h", long_opts, &opt_index)) != -1)
+	{
+		switch (c)
+		{
+		case 'c':
+			if (strnlen(optarg, HOST_NAME_MAX))
+				strncpy(hostname, optarg, HOST_NAME_MAX);
+			break;
+		case 'l':
+			isclient = false;
+			break;
+		case 'p':
+			port = atoi(optarg);
+			break;
+		case 'h':
+			printf(usage, argv[0]);
+			return 0;
+		case '?':
+			printf(usage, argv[0]);
+			return 1;
 		}
 	}
-	if (isclient) {
-		initClientNet(hostname,port);
-	} else {
+	if (isclient)
+	{
+		initClientNet(hostname, port);
+	}
+	else
+	{
 		initServerNet(port);
 	}
 
@@ -445,35 +517,39 @@ int main(int argc, char *argv[])
 	init_ncurses();
 	init_readline();
 	/* start curses thread */
-	if (pthread_create(&tcurses,0,cursesthread,0)) {
+	if (pthread_create(&tcurses, 0, cursesthread, 0))
+	{
 		fprintf(stderr, "Failed to create curses thread.\n");
 	}
 	/* start receiver thread: */
-	if (pthread_create(&trecv,0,recvMsg,0)) {
+	if (pthread_create(&trecv, 0, recvMsg, 0))
+	{
 		fprintf(stderr, "Failed to create update thread.\n");
 	}
 
 	/* put this in the queue to signal need for resize: */
-	redraw_data rd = {false,"","",NULL};
-	do {
+	redraw_data rd = {false, "", "", NULL};
+	do
+	{
 		int c = wgetch(cmd_win);
-		switch (c) {
-			case KEY_RESIZE:
-				pthread_mutex_lock(&qmx);
-				mq.push_back(rd);
-				pthread_cond_signal(&qcv);
-				pthread_mutex_unlock(&qmx);
-				break;
-				// Ctrl-L -- redraw screen
-			// case '\f':
-			// 	// Makes the next refresh repaint the screen from scratch
-			// 	/* XXX this needs to be done in the curses thread as well. */
-			// 	clearok(curscr,true);
-			// 	resize();
-			// 	break;
-			default:
-				input = c;
-				rl_callback_read_char();
+		switch (c)
+		{
+		case KEY_RESIZE:
+			pthread_mutex_lock(&qmx);
+			mq.push_back(rd);
+			pthread_cond_signal(&qcv);
+			pthread_mutex_unlock(&qmx);
+			break;
+			// Ctrl-L -- redraw screen
+		// case '\f':
+		// 	// Makes the next refresh repaint the screen from scratch
+		// 	/* XXX this needs to be done in the curses thread as well. */
+		// 	clearok(curscr,true);
+		// 	resize();
+		// 	break;
+		default:
+			input = c;
+			rl_callback_read_char();
 		}
 	} while (!should_exit);
 
@@ -491,15 +567,17 @@ int main(int argc, char *argv[])
 /* We'll need yet another thread to listen for incoming messages and
  * post them to the queue. */
 
-void* cursesthread(void* pData)
+void *cursesthread(void *pData)
 {
 	/* NOTE: these calls only worked from the main thread... */
 	// init_ncurses();
 	// init_readline();
-	while (true) {
+	while (true)
+	{
 		pthread_mutex_lock(&qmx);
-		while (mq.empty()) {
-			pthread_cond_wait(&qcv,&qmx);
+		while (mq.empty())
+		{
+			pthread_cond_wait(&qcv, &qmx);
 			/* NOTE: pthread_cond_wait will release the mutex and block, then
 			 * reaquire it before returning.  Given that only one thread (this
 			 * one) consumes elements of the queue, we probably don't have to
@@ -508,15 +586,21 @@ void* cursesthread(void* pData)
 		}
 		/* at this point, we have control of the queue, which is not empty,
 		 * so write all the messages and then let go of the mutex. */
-		while (!mq.empty()) {
+		while (!mq.empty())
+		{
 			redraw_data m = mq.front();
 			mq.pop_front();
-			if (m.win == cmd_win) {
+			if (m.win == cmd_win)
+			{
 				cmd_win_redisplay(m.resize);
-			} else if (m.resize) {
+			}
+			else if (m.resize)
+			{
 				resize();
-			} else {
-				msg_win_redisplay(false,m.msg,m.sender);
+			}
+			else
+			{
+				msg_win_redisplay(false, m.msg, m.sender);
 				/* Redraw input window to "focus" it (otherwise the cursor
 				 * will appear in the transcript which is confusing). */
 				cmd_win_redisplay(false);
@@ -527,22 +611,65 @@ void* cursesthread(void* pData)
 	return 0;
 }
 
-void* recvMsg(void*)
+void *recvMsg(void *)
 {
 	size_t maxlen = 256;
-	char msg[maxlen+1];
+	char msg[maxlen + 1];
 	ssize_t nbytes;
-	while (1) {
-		if ((nbytes = recv(sockfd,msg,maxlen,0)) == -1)
+	while (1)
+	{
+		mpz_init(sk_server);
+		mpz_init(pk_server);
+		mpz_init(sk_client);
+		mpz_init(pk_client);
+		// communcate the first part from msg_typed
+		ssize_t y_pk_size;
+		if (recv(sockfd, &y_pk_size, sizeof(y_pk_size), 0) != sizeof(y_pk_size))
+		{
+			// Handle error
+		}
+		char *y_pk_str = (char *)malloc(y_pk_size);
+		if (recv(sockfd, &y_pk_str, y_pk_size, 0) != y_pk_size)
+		{
+			// Handle error
+		}
+		if (mpz_set_str(pk_server, y_pk_str, 10) < 0)
+		{
+			// handle error
+		}
+		// send pk_client
+
+		ssize_t pk_size = mpz_sizeinbase(pk_client, 10);
+
+		// Convert the mpz_t buffer to a string
+		char *pk_str = (char *)malloc(pk_size);
+		mpz_get_str(pk_str, 10, pk_client);
+
+		// Send the buffer size first
+		if (send(sockfd, &pk_size, sizeof(pk_size), 0) != sizeof(pk_size))
+		{
+			// Handle error
+		}
+
+		// Send the buffer itself
+		if (send(sockfd, pk_str, pk_size, 0) != pk_size)
+		{
+			// Handle error
+		}
+
+		//
+		//
+		if ((nbytes = recv(sockfd, msg, maxlen, 0)) == -1)
 			error("recv failed");
 		msg[nbytes] = 0; /* make sure it is null-terminated */
-		if (nbytes == 0) {
+		if (nbytes == 0)
+		{
 			/* signal to the main loop that we should quit: */
 			should_exit = true;
 			return 0;
 		}
 		pthread_mutex_lock(&qmx);
-		mq.push_back({false,msg,"Mr Thread",msg_win});
+		mq.push_back({false, msg, "Mr Thread", msg_win});
 		pthread_cond_signal(&qcv);
 		pthread_mutex_unlock(&qmx);
 	}
